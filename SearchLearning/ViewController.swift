cbef9493b598bc88054eedbb1e48f23aad3d1ccc
Sebastian Wittenkamp <sebastian@bitops.io>
2018-02-03 21:47:43 -0800

    Initial Commit

---
+++ SearchLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchLearning
+//
+//  Created by Sebastian Wittenkamp on 2/3/18.
+//  Copyright © 2018 bitops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
